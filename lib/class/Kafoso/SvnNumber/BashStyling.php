8435ccd2ed4707a0426bb06b2692a5e31e85c144
Kafoso <soefritz@gmail.com>
2015-12-11 14:45:00 +0100

    Added logic for applying backgroundColor and added method 'getMaxTerminalColumns'

--- lib/class/Kafoso/SvnNumber/BashStyling.php
+++ lib/class/Kafoso/SvnNumber/BashStyling.php
@@ -9,12 +9,36 @@ class BashStyling {
         if (is_null($foregroundColor)) {
             $foregroundColor = 231;
         }
-        return "\33[38;5;{$foregroundColor}m{$str}\33[0m";
+        return $this->constructColorSequence($foregroundColor, $backgroundColor) . "{$str}\33[0m";
     }
+
     public function bold($str, $foregroundColor = null, $backgroundColor = null){
         if (is_null($foregroundColor)) {
             $foregroundColor = 231;
         }
-        return "\33[1m\33[38;5;{$foregroundColor}m{$str}\33[0m";
+        return "\33[1m" . $this->constructColorSequence($foregroundColor, $backgroundColor) . "{$str}\33[0m";
+    }
+
+    public function getMaxTerminalColumns(){
+        exec("tput cols", $out);
+        if (is_array($out)) {
+            return intval($out[0]);
+        }
+        return 0;
+    }
+
+    protected function constructColorSequence($foregroundColor, $backgroundColor = null) {
+        $sequence = "";
+        if ($backgroundColor) {
+            $sequence .= sprintf(
+                "\33[48;5;%sm",
+                $backgroundColor
+            );
+        }
+        return sprintf(
+            "%s\33[38;5;%sm",
+            $sequence,
+            $foregroundColor
+        );
     }
 }
