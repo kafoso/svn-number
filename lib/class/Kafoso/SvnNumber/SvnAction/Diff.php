fd25bdd2d4ffc6e186ff99e14a80a51a3aab073c
Kafoso <soefritz@gmail.com>
2015-12-14 15:19:23 +0100

    Improved logic + used color constants

--- lib/class/Kafoso/SvnNumber/SvnAction/Diff.php
+++ lib/class/Kafoso/SvnNumber/SvnAction/Diff.php
@@ -6,46 +6,58 @@ use Kafoso\SvnNumber\Bash\Styling as BashStyling;
 class Diff extends AbstractSvnAction {
     public function getOutputForFilePaths(array $filePaths) {
         $diff = array();
-        foreach ($filePaths as $filePath) {
-            $cmd = "svn di " . escapeshellarg($filePath) . " " . $this->svnNumber->getAdditionalArgsStr();
-            $output = $this->svnNumber->exec($cmd);
-            $diff[] = implode(PHP_EOL, $this->stylize($output));
-        }
-        return implode(PHP_EOL, $diff);
+        $filePaths = array_map(function($filePath){
+            return escapeshellarg($filePath);
+        }, $filePaths);
+        $cmd = sprintf(
+            "svn diff %s %s",
+            implode(" ", $filePaths),
+            $this->svnNumber->getAdditionalArgsStr()
+        );
+        $diffLinesArray = $this->svnNumber->exec($cmd);
+        return implode(PHP_EOL, $this->stylizeDiffLines($diffLinesArray));
     }
 
     public function getOutputAll(){
-        $cmd = "svn di " . $this->svnNumber->getAdditionalArgsStr();
-        $output = $this->svnNumber->exec($cmd);
-        return implode(PHP_EOL, $this->stylize($output));
+        $cmd = "svn diff " . $this->svnNumber->getAdditionalArgsStr();
+        $diffLinesArray = $this->svnNumber->exec($cmd);
+        return implode(PHP_EOL . PHP_EOL, $this->stylizeDiffLines($diffLinesArray));
     }
 
-    protected function stylize(array $svnDiff){
+    protected function stylizeDiffLines(array $diffLinesArray){
+        foreach ($diffLinesArray as &$line) {
+            if (preg_match("/^Index: /", $line)) {
+                $line = PHP_EOL . $line;
+            }
+            $line = $this->stylizeDiff($line);
+        }
+        return $diffLinesArray;
+    }
+
+    protected function stylizeDiff($diff){
         $lineRegexToColor = array(
-            '/^(\=+(\s*))$/' => array(242, null),
-            '/^(\+\+\+\s+.*)$/' => array(40, null),
-            '/^(---\s+.*)$/' => array(160, null),
-            '/^(\+.*)$/' => array(40, null),
-            '/^(-.*)$/' => array(160, null),
-            '/^(@@ .+ @@).*$/' => array(33, null),
-            '/^(Index: .+)$/' => array(226, null)
+            '/^(\=+(\s*))$/' => array(static::COLOR_CODE_GRAY, null),
+            '/^(\+\+\+\s+.*)$/' => array(static::COLOR_CODE_GREEN, null),
+            '/^(---\s+.*)$/' => array(static::COLOR_CODE_RED, null),
+            '/^(\+.*)$/' => array(static::COLOR_CODE_GREEN, null),
+            '/^(-.*)$/' => array(static::COLOR_CODE_RED, null),
+            '/^(@@ .+ @@).*$/' => array(static::COLOR_CODE_TEAL, null),
+            '/^(Index: .+)$/' => array(static::COLOR_CODE_YELLOW, null)
         );
         $bashStyling = new BashStyling;
-        foreach ($svnDiff as &$line) {
-            foreach ($lineRegexToColor as $regex => $colors) {
-                if (preg_match($regex, ltrim($line), $match)) {
-                    list($foreground, $background) = $colors;
-                    $count = 1;
-                    $line = str_replace(
-                        $match[1],
-                        $bashStyling->normal($match[1], $foreground, $background, true),
-                        $line,
-                        $count
-                    );
-                    break;
-                }
+        foreach ($lineRegexToColor as $regex => $colors) {
+            if (preg_match($regex, ltrim($diff), $match)) {
+                list($foreground, $background) = $colors;
+                $count = 1;
+                $diff = str_replace(
+                    $match[1],
+                    $bashStyling->normal($match[1], $foreground, $background, true),
+                    $diff,
+                    $count
+                );
+                break;
             }
         }
-        return $svnDiff;
+        return $diff;
     }
 }
